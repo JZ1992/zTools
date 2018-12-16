560e3f3d46f1fa1e0c8514a8c4e13457976cf56a
JZ1992 <798245215@qq.com>
2018-12-16 19:18:04 +0800

    添加dom节点遍历工具函数

--- zTools.js
+++ zTools.js
@@ -23,6 +23,12 @@
         this.jsonp = jsonp;
         //ajax
         this.zAjax = zAjax;
+        //rem布局解决方案
+        this.remLayoutWeb = remLayoutWeb;
+        this.remLayoutHybrid = remLayoutHybrid;
+
+        //节点遍历
+        this.GoThroughDom = GoThroughDom;
     }
 
 
@@ -252,7 +258,7 @@
             url: '',
             type: 'get',
             data: {},
-            headers:{},
+            headers: {},
             success: function () { },
             error: function () { },
         };
@@ -264,7 +270,7 @@
                 : new ActiveXObject('Microsoft.XMLHTTP');//IE6
             var data = opt.data,
                 url = opt.url,
-                _headers =  opt.headers,
+                _headers = opt.headers,
                 type = opt.type.toUpperCase(),
                 dataArr = [];
             for (var k in data) {
@@ -303,6 +309,215 @@
     }
 
 
+    /**
+     * rem布局
+     * demo基础：750 * 1340 页面
+     * 从网易与淘宝的font-size思考前端设计稿与工作流: http://www.cnblogs.com/lyzg/p/4877277.html
+     */
+    function remLayoutWeb() {
+
+        /**
+          *一、页面开头处引入下面这段代码，用于动态计算font-size[网易做法]
+          */
+        (function (doc, win) {
+            var docEl = doc.documentElement,
+                isIOS = navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
+                dpr = isIOS ? Math.min(win.devicePixelRatio, 3) : 1,
+                dpr = window.top === window.self ? dpr : 1, //被iframe引用时，禁止缩放
+                scale = 1 / dpr,
+                resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize';
+            docEl.dataset.dpr = dpr;
+            var metaEl = doc.createElement('meta');
+            metaEl.name = 'viewport';
+            metaEl.content = 'initial-scale=' + scale + ',maximum-scale=' + scale + ', minimum-scale=' + scale;
+            docEl.firstElementChild.appendChild(metaEl);
+            var recalc = function () {
+                var width = docEl.clientWidth;
+                if (width / dpr > 750) {
+                    width = 750 * dpr;
+                }
+                // 乘以100，px : rem = 100 : 1
+                docEl.style.fontSize = 100 * (width / 750) + 'px';
+                console.log(width, docEl.style.fontSize);
+            };
+            recalc()
+            if (!doc.addEventListener) return;
+            win.addEventListener(resizeEvt, recalc, false);
+        })(document, window);
+
+    }
+
+
+
+    function remLayoutHybrid() {
+        /**
+          *二、淘宝做法（推荐做法，尤其是app内嵌页面）：
+          */
+        ;
+        (function (win, lib) {
+            var doc = win.document;
+            var docEl = doc.documentElement;
+            var metaEl = doc.querySelector('meta[name="viewport"]');
+            var flexibleEl = doc.querySelector('meta[name="flexible"]');
+            var dpr = 0;
+            var scale = 0;
+            var tid;
+            var flexible = lib.flexible || (lib.flexible = {});
+
+            if (metaEl) {
+                var match = metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
+                if (match) {
+                    scale = parseFloat(match[1]);
+                    dpr = parseInt(1 / scale);
+                }
+            } else if (flexibleEl) {
+                var content = flexibleEl.getAttribute('content');
+                if (content) {
+                    var initialDpr = content.match(/initial\-dpr=([\d\.]+)/);
+                    var maximumDpr = content.match(/maximum\-dpr=([\d\.]+)/);
+                    if (initialDpr) {
+                        dpr = parseFloat(initialDpr[1]);
+                        scale = parseFloat((1 / dpr).toFixed(2));
+                    }
+                    if (maximumDpr) {
+                        dpr = parseFloat(maximumDpr[1]);
+                        scale = parseFloat((1 / dpr).toFixed(2));
+                    }
+                }
+            }
+
+            if (!dpr && !scale) {
+                var isAndroid = win.navigator.appVersion.match(/android/gi);
+                var isIPhone = win.navigator.appVersion.match(/iphone/gi);
+                var devicePixelRatio = win.devicePixelRatio;
+                if (isIPhone) {
+                    // iOS下，对于2和3的屏，用2倍的方案，其余的用1倍方案
+                    if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {
+                        dpr = 3;
+                    } else if (devicePixelRatio >= 2 && (!dpr || dpr >= 2)) {
+                        dpr = 2;
+                    } else {
+                        dpr = 1;
+                    }
+                } else {
+                    // 其他设备下，仍旧使用1倍的方案
+                    dpr = 1;
+                }
+                scale = 1 / dpr;
+            }
+
+            docEl.setAttribute('data-dpr', dpr);
+            if (!metaEl) {
+                metaEl = doc.createElement('meta');
+                metaEl.setAttribute('name', 'viewport');
+                metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+                if (docEl.firstElementChild) {
+                    docEl.firstElementChild.appendChild(metaEl);
+                } else {
+                    var wrap = doc.createElement('div');
+                    wrap.appendChild(metaEl);
+                    doc.write(wrap.innerHTML);
+                }
+            }
+
+            function refreshRem() {
+                var width = docEl.getBoundingClientRect().width;
+                // 适配平板
+                if (width / dpr > 540) {
+                    width = 540 * dpr;
+                }
+                var rem = width / 10;
+                docEl.style.fontSize = rem + 'px';
+                flexible.rem = win.rem = rem;
+            }
+
+            win.addEventListener('resize', function () {
+                clearTimeout(tid);
+                tid = setTimeout(refreshRem, 300);
+            }, false);
+            win.addEventListener('pageshow', function (e) {
+                if (e.persisted) {
+                    clearTimeout(tid);
+                    tid = setTimeout(refreshRem, 300);
+                }
+            }, false);
+
+            if (doc.readyState === 'complete') {
+                doc.body.style.fontSize = 12 * dpr + 'px';
+            } else {
+                doc.addEventListener('DOMContentLoaded', function (e) {
+                    doc.body.style.fontSize = 12 * dpr + 'px';
+                }, false);
+            }
+
+
+            refreshRem();
+
+            flexible.dpr = win.dpr = dpr;
+            flexible.refreshRem = refreshRem;
+            flexible.rem2px = function (d) {
+                var val = parseFloat(d) * this.rem;
+                if (typeof d === 'string' && d.match(/rem$/)) {
+                    val += 'px';
+                }
+                return val;
+            }
+            flexible.px2rem = function (d) {
+                var val = parseFloat(d) / this.rem;
+                if (typeof d === 'string' && d.match(/px$/)) {
+                    val += 'rem';
+                }
+                return val;
+            }
+
+        })(window, window['lib'] || (window['lib'] = {}));
+
+    }
+
+
+    //NodeList对象常用方法扩展
+    function NodeListFnExtend() {
+
+    }
+
+
+    /**
+     * dom节点遍历
+     * @param {*} rootNode 
+     * @param {*} fn 
+     */
+    function GoThroughDom(rootNode, fn) {
+        var nodeChildren = rootNode.children;
+        if (nodeChildren.length) {
+            nodeChildren = filterNode(nodeChildren);
+            if (!nodeChildren.length) {
+                return;
+            }
+            nodeChildren.forEach(function (itemNode) {
+                var childrenLenth = itemNode.children ? itemNode.children.length : 0;
+                fn(itemNode);
+                if (childrenLenth) {
+                    GoThroughDom(itemNode,fn);
+                }
+            })
+        } else {
+            fn(rootNode);
+        }
+    }
+
+    //过滤script和css
+    function filterNode(nodeList) {
+        var nodeArray = [];
+        for (var i = 0; i < nodeList.length; i++) {
+            var node = nodeList[i];
+            if (/script|style/.test(node.localName.toLowerCase())) {
+                break;
+            }
+            nodeArray.push(node);
+        }
+        return nodeArray;
+    }
+    //zTools.GoThroughDom(document.body, function(node){console.log(node.localName);})
 
 })(window, document);
 
